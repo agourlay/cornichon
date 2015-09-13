348d4782429d47d5bc8cf7c6eb3149ee11092220
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2015-09-13 19:29:49 +0200

    formatting

--- src/main/scala/com/github/agourlay/cornichon/dsl/HttpDsl.scala
+++ src/main/scala/com/github/agourlay/cornichon/dsl/HttpDsl.scala
@@ -24,13 +24,14 @@ trait HttpDsl extends Dsl {
         if (params.isEmpty) base
         else s"$base with params ${params.mkString(", ")}"
       },
-        action = s ⇒ {
-        val x = this match {
-          case GET    ⇒ Get(url, params, headers)(s)
-          case DELETE ⇒ Delete(url, params, headers)(s)
-        }
-        x.map { case (jsonRes, session) ⇒ (true, session) }.fold(e ⇒ throw e, identity)
-      },
+        action =
+        s ⇒ {
+          val x = this match {
+            case GET    ⇒ Get(url, params, headers)(s)
+            case DELETE ⇒ Delete(url, params, headers)(s)
+          }
+          x.map { case (jsonRes, session) ⇒ (true, session) }.fold(e ⇒ throw e, identity)
+        },
         expected = true
       )
   }
@@ -43,13 +44,14 @@ trait HttpDsl extends Dsl {
         if (params.isEmpty) base
         else s"$base with params ${params.mkString(", ")}"
       },
-        action = s ⇒ {
-        val x = this match {
-          case POST ⇒ Post(dslInput(payload), url, params, headers)(s)
-          case PUT  ⇒ Put(dslInput(payload), url, params, headers)(s)
-        }
-        x.map { case (jsonRes, session) ⇒ (true, session) }.fold(e ⇒ throw e, identity)
-      },
+        action =
+        s ⇒ {
+          val x = this match {
+            case POST ⇒ Post(dslParse(payload), url, params, headers)(s)
+            case PUT  ⇒ Put(dslParse(payload), url, params, headers)(s)
+          }
+          x.map { case (jsonRes, session) ⇒ (true, session) }.fold(e ⇒ throw e, identity)
+        },
         expected = true
       )
   }
@@ -62,13 +64,14 @@ trait HttpDsl extends Dsl {
         if (params.isEmpty) base
         else s"$base with params ${params.mkString(", ")}"
       },
-        action = s ⇒ {
-        val x = this match {
-          case GET_SSE ⇒ GetSSE(url, takeWithin, params, headers)(s)
-          case GET_WS  ⇒ ???
-        }
-        x.map { case (source, session) ⇒ (true, session) }.fold(e ⇒ throw e, identity)
-      },
+        action =
+        s ⇒ {
+          val x = this match {
+            case GET_SSE ⇒ GetSSE(url, takeWithin, params, headers)(s)
+            case GET_WS  ⇒ ???
+          }
+          x.map { case (source, session) ⇒ (true, session) }.fold(e ⇒ throw e, identity)
+        },
         expected = true
       )
   }
@@ -96,25 +99,26 @@ trait HttpDsl extends Dsl {
   def response_is(mapFct: JValue ⇒ JValue, input: String) =
     transform_assert_session(
       key = LastResponseJsonKey,
-      expected = dslInput(input),
-      sessionValue ⇒ mapFct(dslInput(sessionValue)),
+      expected = dslParse(input),
+      sessionValue ⇒ mapFct(dslParse(sessionValue)),
       title = Some(s"HTTP response with transformation is $input")
     )
 
   def response_is(input: String, whiteList: Boolean = false): ExecutableStep[JValue] = {
-    val jsonInput = dslInput(input)
+    val jsonInput = dslParse(input)
     transform_assert_session(
       key = LastResponseJsonKey,
       title = Some(s"HTTP response is $input with whiteList=$whiteList"),
       expected = jsonInput,
-      mapValue = sessionValue ⇒ {
-        val sessionValueJson = dslInput(sessionValue)
-        if (whiteList) {
-          val Diff(changed, _, deleted) = jsonInput.diff(sessionValueJson)
-          if (deleted != JNothing) throw new WhileListError(s"White list error - '$deleted' is not defined in object '$sessionValueJson")
-          if (changed != JNothing) changed else jsonInput
-        } else sessionValueJson
-      }
+      mapValue =
+        sessionValue ⇒ {
+          val sessionValueJson = dslParse(sessionValue)
+          if (whiteList) {
+            val Diff(changed, _, deleted) = jsonInput.diff(sessionValueJson)
+            if (deleted != JNothing) throw new WhileListError(s"White list error - '$deleted' is not defined in object '$sessionValueJson")
+            if (changed != JNothing) changed else jsonInput
+          } else sessionValueJson
+        }
     )
   }
 
@@ -122,33 +126,34 @@ trait HttpDsl extends Dsl {
     transform_assert_session(
       key = LastResponseJsonKey,
       title = titleBuilder(s"HTTP response is '$jsString'", ignoring),
-      expected = dslInput(jsString),
-      mapValue = sessionValue ⇒ {
-        val jsonSessionValue = dslInput(sessionValue)
-        if (ignoring.isEmpty) jsonSessionValue
-        else filterJsonKeys(jsonSessionValue, ignoring)
-      }
+      expected = dslParse(jsString),
+      mapValue =
+        sessionValue ⇒ {
+          val jsonSessionValue = dslParse(sessionValue)
+          if (ignoring.isEmpty) jsonSessionValue
+          else filterJsonKeys(jsonSessionValue, ignoring)
+        }
     )
 
   def response_is(expected: String, ordered: Boolean, ignoring: String*): ExecutableStep[Iterable[JValue]] =
-    dslInput(expected) match {
+    dslParse(expected) match {
       case expectedArray: JArray ⇒
         if (ordered)
           response_array_transform(_.arr.map(filterJsonKeys(_, ignoring)), expectedArray.arr, titleBuilder(s"response array is $expected", ignoring))
         else
           response_array_transform(s ⇒ s.arr.map(filterJsonKeys(_, ignoring)).toSet, expectedArray.arr.toSet, titleBuilder(s"response array not ordered is $expected", ignoring))
       case _ ⇒
-        failWith(new NotAnArrayError(dslInput(expected)), titleBuilder(s"response array is $expected", ignoring).get, Seq.empty[JValue])
+        failWith(new NotAnArrayError(dslParse(expected)), titleBuilder(s"response array is $expected", ignoring).get, Seq.empty[JValue])
     }
 
   def filterJsonKeys(input: JValue, keys: Seq[String]): JValue =
     keys.foldLeft(input)((j, k) ⇒ j.removeField(_._1 == k))
 
   def extract_from_response(extractor: JValue ⇒ JValue, target: String) =
-    extract_from_session(LastResponseJsonKey, s ⇒ extractor(dslInput(s)).values.toString, target)
+    extract_from_session(LastResponseJsonKey, s ⇒ extractor(dslParse(s)).values.toString, target)
 
   def extract_from_response(rootKey: String, target: String) =
-    extract_from_session(LastResponseJsonKey, s ⇒ (dslInput(s) \ rootKey).values.toString, target)
+    extract_from_session(LastResponseJsonKey, s ⇒ (dslParse(s) \ rootKey).values.toString, target)
 
   def show_last_status = show_session(LastResponseStatusKey)
 
@@ -156,24 +161,24 @@ trait HttpDsl extends Dsl {
 
   def show_last_response_headers = show_session(LastResponseHeadersKey)
 
-  private def titleBuilder(baseTitle: String, ignoring: Seq[String]): Option[String] = {
+  private def titleBuilder(baseTitle: String, ignoring: Seq[String]): Option[String] =
     if (ignoring.isEmpty) Some(baseTitle)
     else Some(s"$baseTitle ignoring keys ${ignoring.map(v ⇒ s"'$v'").mkString(", ")}")
-  }
 
   def response_array_transform[A](mapFct: JArray ⇒ A, expected: A, title: Option[String]): ExecutableStep[A] =
     transform_assert_session[A](
       title = title,
       key = LastResponseJsonKey,
       expected = expected,
-      mapValue = sessionValue ⇒ {
-      parse(sessionValue) match {
-        case arr: JArray ⇒
-          logger.debug(s"response_body_array_is applied to ${pretty(render(arr))}")
-          mapFct(arr)
-        case _ ⇒ throw new NotAnArrayError(sessionValue)
+      mapValue =
+      sessionValue ⇒ {
+        dslParse(sessionValue) match {
+          case arr: JArray ⇒
+            logger.debug(s"response_body_array_is applied to ${pretty(render(arr))}")
+            mapFct(arr)
+          case _ ⇒ throw new NotAnArrayError(sessionValue)
+        }
       }
-    }
     )
 
   def response_array_size_is(size: Int) = response_array_transform(_.arr.size, size, Some(s"response array size is $size"))
@@ -182,16 +187,19 @@ trait HttpDsl extends Dsl {
 
   def response_array_does_not_contain(element: String) = response_array_transform(_.arr.contains(parse(element)), false, Some(s"response array does not contain $element"))
 
-  def response_against_schema(schemaUrl: String) = {
-    val jsonSchema = loadJsonSchemaFile(schemaUrl)
-    transform_assert_session(LastResponseJsonKey, Success, sessionValue ⇒ {
-      val jsonNode = mapper.readTree(sessionValue)
-      Try { jsonSchema.validate(jsonNode) }
-    },
-      title = Some(s"HTTP response is valid against JSON schema $schemaUrl"))
-  }
+  def response_against_schema(schemaUrl: String) =
+    transform_assert_session(
+      key = LastResponseJsonKey,
+      expected = Success,
+      mapValue =
+      sessionValue ⇒ {
+        val jsonNode = mapper.readTree(sessionValue)
+        Try { loadJsonSchemaFile(schemaUrl).validate(jsonNode) }
+      },
+      title = Some(s"HTTP response is valid against JSON schema $schemaUrl")
+    )
 
-  private def dslInput[A](input: A): JValue = input match {
+  private def dslParse[A](input: A): JValue = input match {
     case s: String if s.trim.head == '|' ⇒ parse(DataTableParser.parseDataTable(s).asJson.toString())
     case s: String if s.trim.head == '{' ⇒ parse(s)
     case s: String if s.trim.head == '[' ⇒ parse(s)
