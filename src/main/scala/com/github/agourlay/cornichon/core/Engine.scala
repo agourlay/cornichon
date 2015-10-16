2211659ddedc9dc4a86bd7f89a5667af6c8bdbed
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2015-10-16 21:58:25 +0200

    refactoring to extract reusable runSteps fct from Engine

--- src/main/scala/com/github/agourlay/cornichon/core/Engine.scala
+++ src/main/scala/com/github/agourlay/cornichon/core/Engine.scala
@@ -11,53 +11,52 @@ import scala.util._
 class Engine {
 
   def runScenario(scenario: Scenario)(session: Session): ScenarioReport = {
-    @tailrec
-    def loop(steps: Seq[Step], session: Session, eventuallyConf: EventuallyConf, snapshot: Option[RollbackSnapshot], logs: Seq[LogInstruction]): ScenarioReport = {
-      if (steps.isEmpty)
-        SuccessScenarioReport(
-          scenarioName = scenario.name,
-          successSteps = scenario.steps.collect { case ExecutableStep(title, _, _, _) ⇒ title },
-          logs = logs
-        )
-      else
-        steps.head match {
-          case DebugStep(message) ⇒
-            loop(steps.tail, session, eventuallyConf, snapshot, logs :+ ColoredLogInstruction(message(session), CYAN))
-
-          case EventuallyStart(conf) ⇒
-            val updatedLogs = logs :+ DefaultLogInstruction(s"   Eventually bloc with maxDuration = ${conf.maxTime} and interval = ${conf.interval}")
-            loop(steps.tail, session, conf, Some(RollbackSnapshot(steps.tail, session)), updatedLogs)
-
-          case EventuallyStop(conf) ⇒
-            val updatedLogs = logs :+ DefaultLogInstruction(s"   Eventually bloc succeeded in ${conf.maxTime.toSeconds - eventuallyConf.maxTime.toSeconds} sec.")
-            loop(steps.tail, session, EventuallyConf.empty, None, updatedLogs)
-
-          case currentStep: ExecutableStep[_] ⇒
-            val now = System.nanoTime
-            val stepResult = runStepAction(currentStep)(session)
-            val executionTime = Duration.fromNanos(System.nanoTime - now)
-            stepResult match {
-              case Xor.Left(e) ⇒
-                val remainingTime = eventuallyConf.maxTime - executionTime
-                if (remainingTime.gt(Duration.Zero)) {
-                  val updatedLogs = logs ++ logStepErrorResult(currentStep, e, CYAN)
-                  Thread.sleep(eventuallyConf.interval.toMillis)
-                  loop(snapshot.get.steps, snapshot.get.session, eventuallyConf.consume(executionTime + eventuallyConf.interval), snapshot, updatedLogs)
-                } else {
-                  val updatedLogs = logs ++ logStepErrorResult(currentStep, e, RED) ++ logNonExecutedStep(steps.tail)
-                  buildFailedScenarioReport(scenario, steps, currentStep, e, updatedLogs)
-                }
-
-              case Xor.Right(currentSession) ⇒
-                val updatedLogs = if (currentStep.show)
-                  logs :+ ColoredLogInstruction(s"   ${currentStep.title}", GREEN)
-                else logs
-                loop(steps.tail, currentSession, eventuallyConf.consume(executionTime), snapshot, updatedLogs)
-            }
-        }
-    }
     val initLogs = Seq(DefaultLogInstruction(s"Scenario : ${scenario.name}"))
-    loop(scenario.steps, session, EventuallyConf.empty, None, initLogs)
+    runSteps(scenario.steps, session, EventuallyConf.empty, None, initLogs) match {
+      case s @ SuccessRunSteps(_)      ⇒ SuccessScenarioReport(scenario, s)
+      case f @ FailedRunSteps(_, _, _) ⇒ FailedScenarioReport(scenario, f)
+    }
+  }
+
+  @tailrec
+  private def runSteps(steps: Seq[Step], session: Session, eventuallyConf: EventuallyConf, snapshot: Option[RollbackSnapshot], logs: Seq[LogInstruction]): StepsReport = {
+    if (steps.isEmpty) SuccessRunSteps(logs)
+    else
+      steps.head match {
+        case DebugStep(message) ⇒
+          runSteps(steps.tail, session, eventuallyConf, snapshot, logs :+ ColoredLogInstruction(message(session), CYAN))
+
+        case EventuallyStart(conf) ⇒
+          val updatedLogs = logs :+ DefaultLogInstruction(s"   Eventually bloc with maxDuration = ${conf.maxTime} and interval = ${conf.interval}")
+          runSteps(steps.tail, session, conf, Some(RollbackSnapshot(steps.tail, session)), updatedLogs)
+
+        case EventuallyStop(conf) ⇒
+          val updatedLogs = logs :+ DefaultLogInstruction(s"   Eventually bloc succeeded in ${conf.maxTime.toSeconds - eventuallyConf.maxTime.toSeconds} sec.")
+          runSteps(steps.tail, session, EventuallyConf.empty, None, updatedLogs)
+
+        case currentStep: ExecutableStep[_] ⇒
+          val now = System.nanoTime
+          val stepResult = runStepAction(currentStep)(session)
+          val executionTime = Duration.fromNanos(System.nanoTime - now)
+          stepResult match {
+            case Xor.Left(e) ⇒
+              val remainingTime = eventuallyConf.maxTime - executionTime
+              if (remainingTime.gt(Duration.Zero)) {
+                val updatedLogs = logs ++ logStepErrorResult(currentStep, e, CYAN)
+                Thread.sleep(eventuallyConf.interval.toMillis)
+                runSteps(snapshot.get.steps, snapshot.get.session, eventuallyConf.consume(executionTime + eventuallyConf.interval), snapshot, updatedLogs)
+              } else {
+                val updatedLogs = logs ++ logStepErrorResult(currentStep, e, RED) ++ logNonExecutedStep(steps.tail)
+                buildFailedRunSteps(steps, currentStep, e, updatedLogs)
+              }
+
+            case Xor.Right(currentSession) ⇒
+              val updatedLogs = if (currentStep.show)
+                logs :+ ColoredLogInstruction(s"   ${currentStep.title}", GREEN)
+              else logs
+              runSteps(steps.tail, currentSession, eventuallyConf.consume(executionTime), snapshot, updatedLogs)
+          }
+      }
   }
 
   private def runStepAction[A](step: ExecutableStep[A])(implicit session: Session): Xor[CornichonError, Session] =
@@ -95,11 +94,10 @@ class Engine {
         ColoredLogInstruction(s"   ${step.title}", CYAN)
       }
 
-  private def buildFailedScenarioReport(scenario: Scenario, steps: Seq[Step], currentStep: ExecutableStep[_], e: CornichonError, logs: Seq[LogInstruction]): FailedScenarioReport = {
-    val failedStep = FailedStep(currentStep.title, e)
-    val successStep = scenario.steps.takeWhile(_ != steps.head).collect { case ExecutableStep(title, _, _, _) ⇒ title }
+  private def buildFailedRunSteps(steps: Seq[Step], currentStep: ExecutableStep[_], e: CornichonError, logs: Seq[LogInstruction]): FailedRunSteps = {
+    val failedStep = FailedStep(currentStep, e)
     val notExecutedStep = steps.tail.collect { case ExecutableStep(title, _, _, _) ⇒ title }
-    FailedScenarioReport(scenario.name, failedStep, successStep, notExecutedStep, logs)
+    FailedRunSteps(failedStep, notExecutedStep, logs)
   }
 
   private case class RollbackSnapshot(steps: Seq[Step], session: Session)
--- src/main/scala/com/github/agourlay/cornichon/core/ScenarioReport.scala
+++ src/main/scala/com/github/agourlay/cornichon/core/ScenarioReport.scala
@@ -1,5 +1,10 @@
 package com.github.agourlay.cornichon.core
 
+sealed trait StepsReport
+
+case class SuccessRunSteps(logs: Seq[LogInstruction]) extends StepsReport
+case class FailedRunSteps(failedStep: FailedStep, notExecutedStep: Seq[String], logs: Seq[LogInstruction]) extends StepsReport
+
 sealed trait ScenarioReport {
   val scenarioName: String
   val success: Boolean
@@ -10,16 +15,30 @@ case class SuccessScenarioReport(scenarioName: String, successSteps: Seq[String]
   val success = true
 }
 
+object SuccessScenarioReport {
+  def apply(scenario: Scenario, stepsRun: SuccessRunSteps): SuccessScenarioReport = {
+    val successSteps = scenario.steps.collect { case ExecutableStep(title, _, _, _) ⇒ title }
+    SuccessScenarioReport(scenario.name, successSteps, stepsRun.logs)
+  }
+}
+
 case class FailedScenarioReport(scenarioName: String, failedStep: FailedStep, successSteps: Seq[String], notExecutedStep: Seq[String], logs: Seq[LogInstruction]) extends ScenarioReport {
   val success = false
   val msg = s"""
     |
-    |Scenario "$scenarioName" failed at step "${failedStep.step}" with error:
+    |Scenario "$scenarioName" failed at step "${failedStep.step.title}" with error:
     |${failedStep.error.msg}
     | """.trim.stripMargin
 }
 
-case class FailedStep(step: String, error: CornichonError)
+object FailedScenarioReport {
+  def apply(scenario: Scenario, stepsRun: FailedRunSteps): FailedScenarioReport = {
+    val successSteps = scenario.steps.takeWhile(_ != stepsRun.failedStep.step).collect { case ExecutableStep(title, _, _, _) ⇒ title }
+    FailedScenarioReport(scenario.name, stepsRun.failedStep, successSteps, stepsRun.notExecutedStep, stepsRun.logs)
+  }
+}
+
+case class FailedStep(step: ExecutableStep[_], error: CornichonError)
 
 sealed trait LogInstruction {
   val message: String
