8f4b30d3c2a04a6ab303aeee9a0aa5678a63fbf1
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2016-07-24 21:11:06 +0200

    do not evaluate twice message in DebugStep

--- src/main/scala/com/github/agourlay/cornichon/steps/regular/DebugStep.scala
+++ src/main/scala/com/github/agourlay/cornichon/steps/regular/DebugStep.scala
@@ -7,12 +7,12 @@ import scala.util.{ Failure, Success, Try }
 import com.github.agourlay.cornichon.core.Engine._
 
 case class DebugStep(message: Session ⇒ String) extends Step {
-  val title = s"Debug step with message `$message`"
+  val title = s"Debug step"
 
   def run(engine: Engine, session: Session, depth: Int)(implicit ec: ExecutionContext) = {
     Try { message(session) } match {
       case Success(debugMessage) ⇒
-        val runLogs = Vector(DebugLogInstruction(message(session), depth))
+        val runLogs = Vector(DebugLogInstruction(debugMessage, depth))
         SuccessStepsResult(session, runLogs)
       case Failure(e) ⇒
         val runLogs = errorLogs(title, e, depth)
