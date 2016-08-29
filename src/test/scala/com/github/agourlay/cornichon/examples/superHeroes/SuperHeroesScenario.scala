540d3a3b4fab6154f495ac054101e42541bde694
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2016-08-29 14:46:55 +0200

    akka-http 2.4.9

--- src/test/scala/com/github/agourlay/cornichon/examples/superHeroes/SuperHeroesScenario.scala
+++ src/test/scala/com/github/agourlay/cornichon/examples/superHeroes/SuperHeroesScenario.scala
@@ -416,7 +416,7 @@ class SuperHeroesScenario extends CornichonFeature {
           """
         )
 
-        Then assert headers.contain("Server" → "akka-http/2.4.9-RC2")
+        Then assert headers.contain("Server" → "akka-http/2.4.9")
 
         // To make debugging easier, here are some debug steps printing into console
         And I show_session
