65f02525d8dad79839c717ac2a8c05fa61134db6
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2016-11-23 17:42:57 +0100

    add helper assertion

--- src/main/scala/com/github/agourlay/cornichon/core/CornichonError.scala
+++ src/main/scala/com/github/agourlay/cornichon/core/CornichonError.scala
@@ -36,3 +36,7 @@ object CornichonError {
 case class StepExecutionError[A](e: Throwable) extends CornichonError {
   val baseErrorMessage = s"exception thrown '${CornichonError.genStacktrace(e)}'"
 }
+
+case class BasicError(error: String) extends CornichonError {
+  val baseErrorMessage = error
+}
