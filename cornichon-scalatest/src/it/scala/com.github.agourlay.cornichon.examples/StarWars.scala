614c1547d71a7dce34899ca83f02a30adf95c188
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2017-10-03 15:33:35 +0200

    starwars API now uses https

--- cornichon-scalatest/src/it/scala/com.github.agourlay.cornichon.examples/StarWars.scala
+++ cornichon-scalatest/src/it/scala/com.github.agourlay.cornichon.examples/StarWars.scala
@@ -10,7 +10,7 @@ class StarWars extends CornichonFeature {
 
       Scenario("check out Luke Skywalker") {
 
-        When I get("http://swapi.co/api/people/1/")
+        When I get("https://swapi.co/api/people/1/")
 
         Then assert status.is(200)
 
@@ -25,28 +25,28 @@ class StarWars extends CornichonFeature {
             "eye_color": "blue",
             "birth_year": "19BBY",
             "gender": "male",
-            "homeworld": "http://swapi.co/api/planets/1/",
+            "homeworld": "https://swapi.co/api/planets/1/",
             "films": [
-              "http://swapi.co/api/films/2/",
-              "http://swapi.co/api/films/6/",
-              "http://swapi.co/api/films/3/",
-              "http://swapi.co/api/films/1/",
-              "http://swapi.co/api/films/7/"
+              "https://swapi.co/api/films/2/",
+              "https://swapi.co/api/films/6/",
+              "https://swapi.co/api/films/3/",
+              "https://swapi.co/api/films/1/",
+              "https://swapi.co/api/films/7/"
             ],
             "species": [
-              "http://swapi.co/api/species/1/"
+              "https://swapi.co/api/species/1/"
             ],
             "vehicles": [
-              "http://swapi.co/api/vehicles/14/",
-              "http://swapi.co/api/vehicles/30/"
+              "https://swapi.co/api/vehicles/14/",
+              "https://swapi.co/api/vehicles/30/"
             ],
             "starships": [
-              "http://swapi.co/api/starships/12/",
-              "http://swapi.co/api/starships/22/"
+              "https://swapi.co/api/starships/12/",
+              "https://swapi.co/api/starships/22/"
             ],
             "created": "2014-12-09T13:50:51.644000Z",
             "edited": "2014-12-20T21:17:56.891000Z",
-            "url": "http://swapi.co/api/people/1/"
+            "url": "https://swapi.co/api/people/1/"
           }
           """
         )
@@ -68,27 +68,27 @@ class StarWars extends CornichonFeature {
              "surface_water" : "1",
              "population" : "200000",
              "residents" : [
-               "http://swapi.co/api/people/1/",
-               "http://swapi.co/api/people/2/",
-               "http://swapi.co/api/people/4/",
-               "http://swapi.co/api/people/6/",
-               "http://swapi.co/api/people/7/",
-               "http://swapi.co/api/people/8/",
-               "http://swapi.co/api/people/9/",
-               "http://swapi.co/api/people/11/",
-               "http://swapi.co/api/people/43/",
-               "http://swapi.co/api/people/62/"
+               "https://swapi.co/api/people/1/",
+               "https://swapi.co/api/people/2/",
+               "https://swapi.co/api/people/4/",
+               "https://swapi.co/api/people/6/",
+               "https://swapi.co/api/people/7/",
+               "https://swapi.co/api/people/8/",
+               "https://swapi.co/api/people/9/",
+               "https://swapi.co/api/people/11/",
+               "https://swapi.co/api/people/43/",
+               "https://swapi.co/api/people/62/"
              ],
              "films" : [
-               "http://swapi.co/api/films/5/",
-               "http://swapi.co/api/films/4/",
-               "http://swapi.co/api/films/6/",
-               "http://swapi.co/api/films/3/",
-               "http://swapi.co/api/films/1/"
+               "https://swapi.co/api/films/5/",
+               "https://swapi.co/api/films/4/",
+               "https://swapi.co/api/films/6/",
+               "https://swapi.co/api/films/3/",
+               "https://swapi.co/api/films/1/"
              ],
              "created" : "2014-12-09T13:50:49.641000Z",
              "edited" : "2014-12-21T20:48:04.175778Z",
-             "url" : "http://swapi.co/api/planets/1/"
+             "url" : "https://swapi.co/api/planets/1/"
            }
           """
         )
