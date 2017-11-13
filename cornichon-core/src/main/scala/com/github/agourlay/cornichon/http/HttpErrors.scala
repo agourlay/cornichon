bea40856c3a2d0a801c95f3a7d85ac42899ad568
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2017-11-13 13:22:28 +0100

    Don not compute eagerly error message for HttpError.
    Those errors are sometimes scheduled to happen later on, e.g TimeoutError.

--- cornichon-core/src/main/scala/com/github/agourlay/cornichon/http/HttpErrors.scala
+++ cornichon-core/src/main/scala/com/github/agourlay/cornichon/http/HttpErrors.scala
@@ -11,51 +11,54 @@ import scala.concurrent.duration.FiniteDuration
 sealed trait HttpError extends CornichonError
 
 case class TimeoutErrorAfter[A: Show](request: A, after: FiniteDuration) extends HttpError {
-  val baseErrorMessage =
+  lazy val baseErrorMessage =
     s"""|${request.show}
         |connection timed out error after ${after.toMillis} ms""".trim.stripMargin
 }
 
 case class RequestError[A: Show](request: A, e: Throwable) extends HttpError {
-  val baseErrorMessage =
+  lazy val baseErrorMessage =
     s"""|${request.show}
         |encountered the following error:
         |${CornichonError.genStacktrace(e)}""".trim.stripMargin
 }
 
 case class UnmarshallingResponseError(e: Throwable, response: String) extends HttpError {
-  val baseErrorMessage = s"""HTTP response '$response' generated error:
+  lazy val baseErrorMessage = s"""HTTP response '$response' generated error:
                |${CornichonError.genStacktrace(e)}""".trim.stripMargin
 }
 
 case class SseError(e: Throwable) extends HttpError {
-  val baseErrorMessage =
+  lazy val baseErrorMessage =
     s"""expected SSE connection but got error:
        |${CornichonError.genStacktrace(e)}""".trim.stripMargin
 }
 
 case class WsUpgradeError(status: Int) extends HttpError {
-  val baseErrorMessage = s"Websocket upgrade error - status received '$status'"
+  lazy val baseErrorMessage = s"Websocket upgrade error - status received '$status'"
 }
 
 //TODO unify with StatusSteps assertion
 case class StatusNonExpected(expected: Int, response: CornichonHttpResponse) extends HttpError {
-  val baseErrorMessage =
+  lazy val baseErrorMessage =
     s"""expected status code '$expected' but '${response.status}' was received with body:
        |${parseJsonUnsafe(response.body).show}""".stripMargin
 }
 
 case class MalformedHeadersError(error: String) extends CornichonError {
-  val baseErrorMessage = s"""parsing headers generated error:
-               |$error""".trim.stripMargin
+  lazy val baseErrorMessage =
+    s"""parsing headers generated error:
+       |$error""".trim.stripMargin
 }
 
 case class MalformedUriError(uri: String, error: String) extends CornichonError {
-  val baseErrorMessage = s"""parsing URI '$uri' generated error:
-                            |$error""".trim.stripMargin
+  lazy val baseErrorMessage =
+    s"""parsing URI '$uri' generated error:
+       |$error""".trim.stripMargin
 }
 
 case class BadSessionHeadersEncoding(header: String) extends CornichonError {
-  val baseErrorMessage = s"""header '$header' does not respect the session encoding convention.
-                            |Hint: use HttpService.encodeSessionHeaders to properly encode your headers in the Session.""".trim.stripMargin
+  lazy val baseErrorMessage =
+    s"""header '$header' does not respect the session encoding convention.
+       |Hint: use HttpService.encodeSessionHeaders to properly encode your headers in the Session.""".trim.stripMargin
 }
