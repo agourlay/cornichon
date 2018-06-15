e57a7997852a93b8ba7b426d93e75aebe2647c58
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2018-06-15 17:01:54 +0200

    decrease allocations in macro

--- cornichon-core/src/main/scala/com/github/agourlay/cornichon/macros/package.scala
+++ cornichon-core/src/main/scala/com/github/agourlay/cornichon/macros/package.scala
@@ -8,25 +8,26 @@ import scala.reflect.macros.{ TypecheckException, blackbox }
 
 package object macros {
   class Macro(context: blackbox.Context) {
-    val c = context
+    val c: blackbox.Context = context
 
     import c.universe._
 
-    def collectImpl(body: Tree) = {
+    def collectImpl(body: Tree): c.universe.Tree = {
       val contextType = c.prefix.tree.tpe
       val validContext = typeOf[BodyElementCollector[_, _]]
 
-      if (!(contextType <:< validContext)) {
+      if (!(contextType <:< validContext))
         c.abort(c.enclosingPosition, s"Macro only allowed to be used directly inside of a `$validContext`")
-      } else {
+      else {
         val elementType = contextType.typeArgs.head
 
-        blockOrApplyExpressionList(body, elementType, s"Unsupported expression. Only expressions of type `$elementType` are allowed here.") { elements ⇒
-          val (finalTree, rest) = elements.foldLeft((q"List.empty": Tree) → List.empty[Tree]) {
-            case ((accTree, accSingle), elem) if elem.tpe <:< elementType ⇒
-              accTree → (accSingle :+ elem)
+        blockOrApplyExpressionList(body, elementType) { elements ⇒
+          val (finalTree, rest) = elements.foldLeft[(Tree, List[Tree])]((q"Nil": Tree) → Nil) {
             case ((accTree, accSingle), elem) ⇒
-              q"$accTree ++ $accSingle ++ $elem" → Nil
+              if (elem.tpe <:< elementType)
+                accTree → (accSingle :+ elem)
+              else
+                q"$accTree ++ $accSingle ++ $elem" → Nil
           }
 
           q"${c.prefix.tree}.get($finalTree ++ $rest)"
@@ -34,43 +35,42 @@ package object macros {
       }
     }
 
-    private def blockOrApplyExpressionList(body: Tree, elementType: Type, unsupportedMessage: String)(typesTreesFn: List[Tree] ⇒ Tree) =
+    private def blockOrApplyExpressionList(body: Tree, elementType: Type)(typesTreesFn: List[Tree] ⇒ Tree): c.universe.Tree =
       body match {
         case block: Block ⇒
           blockExpressionList(block, elementType)(typesTreesFn)
         case app: Apply ⇒
-          singleExpressionList(app, elementType, typesTreesFn)
+          singleExpressionList(app, elementType)(typesTreesFn)
         case Typed(app: Apply, _) ⇒
-          singleExpressionList(app, elementType, typesTreesFn)
+          singleExpressionList(app, elementType)(typesTreesFn)
         case s @ Select(_, _) ⇒
-          singleExpressionList(s, elementType, typesTreesFn)
+          singleExpressionList(s, elementType)(typesTreesFn)
         case e ⇒
+          val unsupportedMessage = s"Unsupported expression. Only expressions of type `$elementType` are allowed here."
           c.abort(e.pos, s"$unsupportedMessage\nfound '$e' of type '${e.tpe}'")
       }
 
-    private def singleExpressionList(app: Tree, elementType: Type, typesTreesFn: List[Tree] ⇒ Tree) =
-      typecheck(app, elementType) match {
+    private def singleExpressionList(app: Tree, elementType: Type)(typesTreesFn: List[Tree] ⇒ Tree): c.universe.Tree =
+      typeCheck(elementType)(app) match {
         case Right(checked)     ⇒ typesTreesFn(checked :: Nil)
         case Left((pos, error)) ⇒ c.abort(pos, error)
       }
 
-    private def blockExpressionList(block: Block, elementType: Type)(typesTreesFn: List[Tree] ⇒ Tree) = {
+    private def blockExpressionList(block: Block, elementType: Type)(typesTreesFn: List[Tree] ⇒ Tree): c.universe.Tree = {
       val allStats = block.stats :+ block.expr
-      val checked = allStats map (typecheck(_, elementType))
-      val errors = checked.collect { case Left(error) ⇒ error }
+      val checked = allStats.map(typeCheck(elementType))
 
-      if (errors.nonEmpty) {
-        errors.dropRight(1).foreach {
-          case (pos, error) ⇒ c.error(pos, error)
-        }
-
-        c.abort(errors.last._1, errors.last._2)
+      if (checked.exists(_.isLeft)) {
+        val errors = checked.collect { case Left(error) ⇒ error }
+        errors.dropRight(1).foreach { case (pos, error) ⇒ c.error(pos, error) }
+        val lastError = errors.last
+        c.abort(lastError._1, lastError._2)
       } else
         typesTreesFn(checked.collect { case Right(typed) ⇒ typed })
     }
 
-    private def typecheck(tree: Tree, elementType: Type) = {
-      val seq = seqType(elementType)
+    private def typeCheck(elementType: Type)(tree: Tree): Either[(c.universe.Position, String), c.Tree] = {
+      val seq = c.typecheck(q"Seq[$elementType]()").tpe
       val checked = c.typecheck(tree)
       // checked.tpe is null if the statement is an import
       if (checked.tpe == null)
@@ -83,8 +83,5 @@ package object macros {
             (s"Result of this expression can be either `$elementType` or `$seq`. " + msg))
         }
     }
-
-    private def seqType(elementType: Type) =
-      c.typecheck(q"Seq[$elementType]()").tpe
   }
 }
