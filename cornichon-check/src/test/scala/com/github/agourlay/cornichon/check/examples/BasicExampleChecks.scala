b888488f84e9031b3f8f8ac13aee6337b5444501
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2018-10-30 14:15:53 +0100

    add more post conditions

--- cornichon-check/src/test/scala/com/github/agourlay/cornichon/check/examples/BasicExampleChecks.scala
+++ cornichon-check/src/test/scala/com/github/agourlay/cornichon/check/examples/BasicExampleChecks.scala
@@ -6,24 +6,16 @@ import com.github.agourlay.cornichon.steps.regular.EffectStep
 
 class BasicExampleChecks extends CornichonFeature with CheckDsl {
 
-  def integerGen(rc: RandomContext): ValueGenerator[Int] = ValueGenerator(
-    name = "Integer",
-    genFct = () ⇒ rc.seededRandom.nextInt(100))
-
   def stringGen(rc: RandomContext): ValueGenerator[String] = ValueGenerator(
     name = "String",
     genFct = () ⇒ rc.seededRandom.alphanumeric.take(20).mkString(""))
 
-  // Those examples are only here to illustrate the API.
-  // `cornichon-check` is to be used for more complex state-machine
-  // often involving communication with a remote server.
   def feature = Feature("Basic examples of checks") {
 
     Scenario("reverse string") {
-      Given I check_model(maxNumberOfRuns = 10, maxNumberOfTransitions = 1)(
+      Given I check_model(maxNumberOfRuns = 5, maxNumberOfTransitions = 1)(
         modelRunner = ModelRunner.make[String](stringGen)(
           model = {
-
             val generateStringAction = Action1[String](
               description = "generate a string",
               preConditions = session_value("random-input").isAbsent :: Nil,
@@ -31,7 +23,7 @@ class BasicExampleChecks extends CornichonFeature with CheckDsl {
                 val randomString = g()
                 EffectStep.fromSyncE("save random string", _.addValue("random-input", randomString))
               },
-              postConditions = Nil)
+              postConditions = session_value("random-input").isPresent :: Nil)
 
             val reverseAction = Action1[String](
               description = "reverse a string",
@@ -43,7 +35,7 @@ class BasicExampleChecks extends CornichonFeature with CheckDsl {
                   s1 ← s.addValue("reversed-random-input", reversed)
                 } yield s1
               }),
-              postConditions = Nil)
+              postConditions = session_value("reversed-random-input").isPresent :: Nil)
 
             Model(
               description = "reversing a string",
