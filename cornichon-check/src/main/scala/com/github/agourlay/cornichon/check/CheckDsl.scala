24f2871c6528b1593b46400b85ccf548c3ab9dad
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2018-11-16 17:27:40 +0100

    support different forAll arity

--- cornichon-check/src/main/scala/com/github/agourlay/cornichon/check/CheckDsl.scala
+++ cornichon-check/src/main/scala/com/github/agourlay/cornichon/check/CheckDsl.scala
@@ -1,32 +1,43 @@
 package com.github.agourlay.cornichon.check
 
 import com.github.agourlay.cornichon.check.checkModel.{ CheckModelStep, ModelRunner }
-import com.github.agourlay.cornichon.check.forAll.ForAllStep1
+import com.github.agourlay.cornichon.check.forAll.ForAllStep
 import com.github.agourlay.cornichon.core.Step
+import com.github.agourlay.cornichon.check.NoValue.seededNoValueGenerator
 
 trait CheckDsl {
 
   def check_model[A, B, C, D, E, F](maxNumberOfRuns: Int, maxNumberOfTransitions: Int, seed: Option[Long] = None)(modelRunner: ModelRunner[A, B, C, D, E, F]): Step =
     CheckModelStep(maxNumberOfRuns, maxNumberOfTransitions, modelRunner, seed)
 
-  def for_all[A](description: String, maxNumberOfRuns: Int, ga: RandomContext ⇒ Generator[A])(f: A ⇒ Step): Step =
-    new ForAllStep1[A](description, maxNumberOfRuns)(ga, f)
+  def for_all[A](description: String, maxNumberOfRuns: Int, ga: RandomContext ⇒ Generator[A])(builder: A ⇒ Step): Step = {
+    val g: A ⇒ NoValue ⇒ NoValue ⇒ NoValue ⇒ NoValue ⇒ NoValue ⇒ Step = { a: A ⇒ _ ⇒ _ ⇒ _ ⇒ _ ⇒ _ ⇒ builder(a) }
+    new ForAllStep[A, NoValue, NoValue, NoValue, NoValue, NoValue](description, maxNumberOfRuns)(ga, seededNoValueGenerator, seededNoValueGenerator, seededNoValueGenerator, seededNoValueGenerator, seededNoValueGenerator)(g)
+  }
 
-  // TODO Find a clean way to not have 6 concrete step implementations
+  def for_all[A, B](description: String, maxNumberOfRuns: Int, ga: RandomContext ⇒ Generator[A], gb: RandomContext ⇒ Generator[B])(builder: A ⇒ B ⇒ Step): Step = {
+    val g: A ⇒ B ⇒ NoValue ⇒ NoValue ⇒ NoValue ⇒ NoValue ⇒ Step = { a: A ⇒ b: B ⇒ _ ⇒ _ ⇒ _ ⇒ _ ⇒ builder(a)(b) }
+    new ForAllStep[A, B, NoValue, NoValue, NoValue, NoValue](description, maxNumberOfRuns)(ga, gb, seededNoValueGenerator, seededNoValueGenerator, seededNoValueGenerator, seededNoValueGenerator)(g)
+  }
 
-  def for_all[A, B](description: String, maxNumberOfRuns: Int, ga: RandomContext ⇒ Generator[A], gb: RandomContext ⇒ Generator[B])(f: A ⇒ B ⇒ Step): Step =
-    ???
+  def for_all[A, B, C](description: String, maxNumberOfRuns: Int, ga: RandomContext ⇒ Generator[A], gb: RandomContext ⇒ Generator[B], gc: RandomContext ⇒ Generator[C])(builder: A ⇒ B ⇒ C ⇒ Step): Step = {
+    val g: A ⇒ B ⇒ C ⇒ NoValue ⇒ NoValue ⇒ NoValue ⇒ Step = { a: A ⇒ b: B ⇒ c: C ⇒ _ ⇒ _ ⇒ _ ⇒ builder(a)(b)(c) }
+    new ForAllStep[A, B, C, NoValue, NoValue, NoValue](description, maxNumberOfRuns)(ga, gb, gc, seededNoValueGenerator, seededNoValueGenerator, seededNoValueGenerator)(g)
+  }
 
-  def for_all[A, B, C](description: String, maxNumberOfRuns: Int, ga: RandomContext ⇒ Generator[A], gb: RandomContext ⇒ Generator[B], gc: RandomContext ⇒ Generator[C])(f: A ⇒ B ⇒ C ⇒ Step): Step =
-    ???
+  def for_all[A, B, C, D](description: String, maxNumberOfRuns: Int, ga: RandomContext ⇒ Generator[A], gb: RandomContext ⇒ Generator[B], gc: RandomContext ⇒ Generator[C], gd: RandomContext ⇒ Generator[D])(builder: A ⇒ B ⇒ C ⇒ D ⇒ Step): Step = {
+    val g: A ⇒ B ⇒ C ⇒ D ⇒ NoValue ⇒ NoValue ⇒ Step = { a: A ⇒ b: B ⇒ c: C ⇒ d: D ⇒ _ ⇒ _ ⇒ builder(a)(b)(c)(d) }
+    new ForAllStep[A, B, C, D, NoValue, NoValue](description, maxNumberOfRuns)(ga, gb, gc, gd, seededNoValueGenerator, seededNoValueGenerator)(g)
+  }
 
-  def for_all[A, B, C, D](description: String, maxNumberOfRuns: Int, ga: RandomContext ⇒ Generator[A], gb: RandomContext ⇒ Generator[B], gc: RandomContext ⇒ Generator[C], gd: RandomContext ⇒ Generator[D])(f: A ⇒ B ⇒ C ⇒ D ⇒ Step): Step =
-    ???
+  def for_all[A, B, C, D, E](description: String, maxNumberOfRuns: Int, ga: RandomContext ⇒ Generator[A], gb: RandomContext ⇒ Generator[B], gc: RandomContext ⇒ Generator[C], gd: RandomContext ⇒ Generator[D], ge: RandomContext ⇒ Generator[E])(builder: A ⇒ B ⇒ C ⇒ D ⇒ E ⇒ Step): Step = {
+    val g: A ⇒ B ⇒ C ⇒ D ⇒ E ⇒ NoValue ⇒ Step = { a: A ⇒ b: B ⇒ c: C ⇒ d: D ⇒ e: E ⇒ _ ⇒ builder(a)(b)(c)(d)(e) }
+    new ForAllStep[A, B, C, D, E, NoValue](description, maxNumberOfRuns)(ga, gb, gc, gd, ge, seededNoValueGenerator)(g)
+  }
 
-  def for_all[A, B, C, D, E](description: String, maxNumberOfRuns: Int, ga: RandomContext ⇒ Generator[A], gb: RandomContext ⇒ Generator[B], gc: RandomContext ⇒ Generator[C], gd: RandomContext ⇒ Generator[D], ge: RandomContext ⇒ Generator[E])(f: A ⇒ B ⇒ C ⇒ D ⇒ E ⇒ Step): Step =
-    ???
-
-  def for_all[A, B, C, D, E, F](description: String, maxNumberOfRuns: Int, ga: RandomContext ⇒ Generator[A], gb: RandomContext ⇒ Generator[B], gc: RandomContext ⇒ Generator[C], gd: RandomContext ⇒ Generator[D], ge: RandomContext ⇒ Generator[E], gf: RandomContext ⇒ Generator[F])(f: A ⇒ B ⇒ C ⇒ D ⇒ E ⇒ F ⇒ Step): Step =
-    ???
+  def for_all[A, B, C, D, E, F](description: String, maxNumberOfRuns: Int, ga: RandomContext ⇒ Generator[A], gb: RandomContext ⇒ Generator[B], gc: RandomContext ⇒ Generator[C], gd: RandomContext ⇒ Generator[D], ge: RandomContext ⇒ Generator[E], gf: RandomContext ⇒ Generator[F])(builder: A ⇒ B ⇒ C ⇒ D ⇒ E ⇒ F ⇒ Step): Step = {
+    val g: A ⇒ B ⇒ C ⇒ D ⇒ E ⇒ F ⇒ Step = { a: A ⇒ b: B ⇒ c: C ⇒ d: D ⇒ e: E ⇒ f: F ⇒ builder(a)(b)(c)(d)(e)(f) }
+    new ForAllStep[A, B, C, D, E, F](description, maxNumberOfRuns)(ga, gb, gc, gd, ge, gf)(g)
+  }
 
 }
--- cornichon-check/src/main/scala/com/github/agourlay/cornichon/check/forAll/ForAllStep.scala
+++ cornichon-check/src/main/scala/com/github/agourlay/cornichon/check/forAll/ForAllStep.scala
@@ -1,6 +1,6 @@
 package com.github.agourlay.cornichon.check.forAll
 
-import com.github.agourlay.cornichon.check.{ Generator, RandomContext }
+import com.github.agourlay.cornichon.check.{ Generator, NoValueGenerator, RandomContext }
 import com.github.agourlay.cornichon.core.Done.rightDone
 import com.github.agourlay.cornichon.core.core.StepResult
 import com.github.agourlay.cornichon.core._
@@ -9,7 +9,7 @@ import monix.eval.Task
 
 import scala.util.Random
 
-class ForAllStep1[A](description: String, maxNumberOfRuns: Int, withSeed: Option[Long] = None)(ga: RandomContext ⇒ Generator[A], f: A ⇒ Step) extends WrapperStep {
+class ForAllStep[A, B, C, D, E, F](description: String, maxNumberOfRuns: Int, withSeed: Option[Long] = None)(ga: RandomContext ⇒ Generator[A], gb: RandomContext ⇒ Generator[B], gc: RandomContext ⇒ Generator[C], gd: RandomContext ⇒ Generator[D], ge: RandomContext ⇒ Generator[E], gf: RandomContext ⇒ Generator[F])(f: A ⇒ B ⇒ C ⇒ D ⇒ E ⇒ F ⇒ Step) extends WrapperStep {
 
   private val randomContext = {
     val seed = withSeed.getOrElse(System.currentTimeMillis())
@@ -18,17 +18,32 @@ class ForAllStep1[A](description: String, maxNumberOfRuns: Int, withSeed: Option
   }
 
   val genA = ga(randomContext)
-  val baseTitle = s"ForAll '${genA.name}' check '$description'"
+  val genB = gb(randomContext)
+  val genC = gc(randomContext)
+  val genD = gd(randomContext)
+  val genE = ge(randomContext)
+  val genF = gf(randomContext)
+
+  val concreteGens = List(genA, genB, genC, genD, genE, genF).filter(_ != NoValueGenerator)
+
+  val baseTitle = s"ForAll '${concreteGens.map(_.name).mkString(",")}' check '$description'"
   val title = s"$baseTitle with maxNumberOfRuns=$maxNumberOfRuns and seed=${randomContext.seed}"
 
   private def repeatModelOnSuccess(runNumber: Int)(engine: Engine, initialRunState: RunState): Task[(RunState, Either[FailedStep, Done])] =
     if (runNumber > maxNumberOfRuns)
       Task.now((initialRunState, rightDone))
     else {
-      val generated = genA.value(initialRunState.session)()
-      val preRunLog = InfoLogInstruction(s"Run #$runNumber [${genA.name} -> $generated]", initialRunState.depth)
+      val generatedA = genA.value(initialRunState.session)()
+      val generatedB = genB.value(initialRunState.session)()
+      val generatedC = genC.value(initialRunState.session)()
+      val generatedD = genD.value(initialRunState.session)()
+      val generatedE = genE.value(initialRunState.session)()
+      val generatedF = genF.value(initialRunState.session)()
+
+      val preRunLog = InfoLogInstruction(s"Run #$runNumber", initialRunState.depth)
       val invariantRunState = initialRunState.nestedContext.appendLog(preRunLog)
-      val invariantStep = f(generated)
+      val invariantStep = f(generatedA)(generatedB)(generatedC)(generatedD)(generatedE)(generatedF)
+
       invariantStep.run(engine)(invariantRunState).flatMap {
         case (newState, l @ Left(_)) ⇒
           val postRunLog = InfoLogInstruction(s"Run #$runNumber - Failed", initialRunState.depth)
