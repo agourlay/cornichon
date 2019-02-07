9654bcf4a0ee80216cc24d1e9fd0e4bfef60df91
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2019-02-07 11:01:57 +0100

    add experimental GenericCollectionAssertStepBuilder

---
+++ cornichon-core/src/main/scala/com/github/agourlay/cornichon/steps/regular/assertStep/GenericCollectionAssertStepBuilder.scala
@@ -0,0 +1,44 @@
+package com.github.agourlay.cornichon.steps.regular.assertStep
+
+import cats.{Eq, Order, Show}
+import cats.instances.int._
+import com.github.agourlay.cornichon.core.{CornichonError, Session}
+
+//experimental and unused ;)
+abstract class GenericCollectionAssertStepBuilder[A: Show: Order: Eq: Diff] { outer =>
+
+  protected val baseTitle: String
+  protected def sessionExtractor(s: Session): Either[CornichonError, Iterable[A]]
+
+  def size : GenericAssertStepBuilder[Int] = new GenericAssertStepBuilder[Int] {
+    protected val baseTitle: String =  s"$baseTitle's size"
+    protected def sessionExtractor(s: Session): Either[CornichonError, (Int, Option[() => String])] = outer.sessionExtractor(s).map(c => (c.seq.size, None))
+  }
+
+  def isNotEmpty: AssertStep = {
+    val fullTitle = s"$baseTitle is not empty"
+    AssertStep(
+      title = fullTitle,
+      action = s ⇒ Assertion.either {
+        sessionExtractor(s).map { asserted ⇒
+          CollectionNotEmptyAssertion(asserted, baseTitle)
+        }
+      }
+    )
+  }
+
+  def isEmpty: AssertStep = {
+    val fullTitle = s"$baseTitle is empty"
+    AssertStep(
+      title = fullTitle,
+      action = s ⇒ Assertion.either {
+        sessionExtractor(s).map { asserted ⇒
+          CollectionEmptyAssertion(asserted, baseTitle)
+        }
+      }
+    )
+  }
+
+
+}
+
