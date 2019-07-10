fc3af2b7b7b8b3f0c33289ebef0eea4f08ff5886
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2019-07-10 12:08:44 +0200

    http4s client now support SSE

--- cornichon-core/src/main/scala/com/github/agourlay/cornichon/http/client/Http4sClient.scala
+++ cornichon-core/src/main/scala/com/github/agourlay/cornichon/http/client/Http4sClient.scala
@@ -2,12 +2,16 @@ package com.github.agourlay.cornichon.http.client
 
 import cats.data.EitherT
 import cats.syntax.either._
+import cats.syntax.show._
 import com.github.agourlay.cornichon.core.{ CornichonError, CornichonException, Done }
 import com.github.agourlay.cornichon.http.HttpMethods._
 import com.github.agourlay.cornichon.http._
 import com.github.agourlay.cornichon.http.HttpService._
+import com.github.agourlay.cornichon.http.HttpStreams.SSE
 import com.github.agourlay.cornichon.util.Caching
 import io.circe.Json
+import io.circe.generic.auto._
+import io.circe.syntax._
 import monix.eval.Task
 import monix.eval.Task._
 import monix.execution.Scheduler
@@ -39,7 +43,7 @@ class Http4sClient(scheduler: Scheduler, ec: ExecutionContext) extends HttpClien
       .map { case (client, shutdown) ⇒ GZip()(client) -> shutdown } // always adds `Accept-Encoding` `gzip`
       .runSyncUnsafe(10.seconds)
 
-  private def httpMethodMapper(method: HttpMethod): Method = method match {
+  private def toHttp4sMethod(method: HttpMethod): Method = method match {
     case DELETE  ⇒ org.http4s.Method.DELETE
     case GET     ⇒ org.http4s.Method.GET
     case HEAD    ⇒ org.http4s.Method.HEAD
@@ -50,10 +54,13 @@ class Http4sClient(scheduler: Scheduler, ec: ExecutionContext) extends HttpClien
     case other   ⇒ throw CornichonException(s"unsupported HTTP method ${other.name}")
   }
 
-  private def buildHeaders(headers: Seq[(String, String)]): Headers = {
+  private def toHttp4sHeaders(headers: Seq[(String, String)]): Headers = {
     val h: List[Header] = headers.map { case (n, v) ⇒ Header(n, v).parsed }(breakOut)
     Headers(h)
   }
+
+  private def fromHttp4sHeaders(headers: Headers): Seq[(String, String)] =
+    headers.toList.map(h ⇒ (h.name.value, h.value))(breakOut)
 
   private def addQueryParams(uri: Uri, moreParams: Seq[(String, String)]): Uri =
     if (moreParams.isEmpty)
@@ -64,40 +71,75 @@ class Http4sClient(scheduler: Scheduler, ec: ExecutionContext) extends HttpClien
       uri.copy(query = Query.fromVector(uri.query.toVector ++ q.toVector))
     }
 
-  private def handleResponse[A](response: Response[Task]): Task[CornichonHttpResponse] = {
-    response
-      .bodyAsText
-      .compile
-      .fold("")(_ ++ _)
-      .map { decodedBody ⇒
-        CornichonHttpResponse(
-          status = response.status.code,
-          headers = response.headers.toList.map(h ⇒ (h.name.value, h.value))(breakOut),
-          body = decodedBody
-        )
-      }
-  }
-
   override def runRequest(cReq: HttpRequest[Json], t: FiniteDuration): EitherT[Task, CornichonError, CornichonHttpResponse] =
     parseUri(cReq.url).fold(
       e ⇒ EitherT.left[CornichonHttpResponse](Task.now(e)),
       uri ⇒ EitherT {
-        val req = Request[Task](httpMethodMapper(cReq.method))
-          .withHeaders(buildHeaders(cReq.headers))
+        val req = Request[Task](toHttp4sMethod(cReq.method))
+          .withHeaders(toHttp4sHeaders(cReq.headers))
           .withUri(addQueryParams(uri, cReq.params))
 
         val completeRequest = cReq.body.fold(req)(b ⇒ req.withEntity(b))
-        val response = httpClient.fetch(completeRequest)(handleResponse).map(_.asRight[CornichonError])
+        val cornichonResponse = httpClient.fetch(completeRequest) { http4sResp ⇒
+          http4sResp
+            .bodyAsText
+            .compile
+            .fold("")(_ ++ _)
+            .map { decodedBody ⇒
+              CornichonHttpResponse(
+                status = http4sResp.status.code,
+                headers = fromHttp4sHeaders(http4sResp.headers),
+                body = decodedBody
+              ).asRight[CornichonError]
+            }
+        }
+
         val timeout = Task.delay(TimeoutErrorAfter(cReq, t).asLeft).delayExecution(t)
 
-        Task.race(response, timeout)
+        Task.race(cornichonResponse, timeout)
           .map(_.fold(identity, identity))
           .onErrorRecover { case t: Throwable ⇒ RequestError(cReq, t).asLeft }
       }
     )
 
-  // TODO SSE support https://github.com/http4s/http4s/issues/619
-  def openStream(req: HttpStreamedRequest, t: FiniteDuration): Task[Either[CornichonError, CornichonHttpResponse]] = ???
+  private def runSSE(streamReq: HttpStreamedRequest, t: FiniteDuration): EitherT[Task, CornichonError, CornichonHttpResponse] = {
+    parseUri(streamReq.url).fold(
+      e ⇒ EitherT.left[CornichonHttpResponse](Task.now(e)),
+      uri ⇒ EitherT {
+        val req = Request[Task](org.http4s.Method.GET)
+          .withHeaders(toHttp4sHeaders(streamReq.addHeaders("text" → "event-stream").headers))
+          .withUri(addQueryParams(uri, streamReq.params))
+
+        val cornichonResponse = httpClient.fetch(req) { http4sResp ⇒
+          http4sResp
+            .body
+            .through(ServerSentEvent.decoder)
+            .interruptAfter(streamReq.takeWithin)
+            .compile
+            .toList
+            .map { events ⇒
+              CornichonHttpResponse(
+                status = http4sResp.status.code,
+                headers = fromHttp4sHeaders(http4sResp.headers),
+                body = Json.fromValues(events.map(_.asJson)).show
+              ).asRight[CornichonError]
+            }
+        }
+
+        val timeout = Task.delay(TimeoutErrorAfter(streamReq, t).asLeft).delayExecution(t)
+
+        Task.race(cornichonResponse, timeout)
+          .map(_.fold(identity, identity))
+          .onErrorRecover { case t: Throwable ⇒ RequestError(streamReq, t).asLeft }
+      }
+    )
+  }
+
+  def openStream(req: HttpStreamedRequest, t: FiniteDuration): Task[Either[CornichonError, CornichonHttpResponse]] =
+    req.stream match {
+      case SSE ⇒ runSSE(req, t).value
+      case _   ⇒ ??? // TODO implement WS support
+    }
 
   def shutdown(): Task[Done] =
     safeShutdown.map { _ ⇒ uriCache.invalidateAll(); Done }
@@ -110,5 +152,4 @@ class Http4sClient(scheduler: Scheduler, ec: ExecutionContext) extends HttpClien
 
   private def parseUri(uri: String): Either[CornichonError, Uri] =
     uriCache.get(uri, u ⇒ Uri.fromString(u).leftMap(e ⇒ MalformedUriError(u, e.message)))
-
 }
