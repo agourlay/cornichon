f73fb8f954360e3d9023ff8e74428cd6e21bf991
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2019-08-22 11:35:13 +0200

    remove scalac warning about package object by moving the macro to the dsl package

--- cornichon-core/src/main/scala/com/github/agourlay/cornichon/dsl/BodyElementCollector.scala
+++ cornichon-core/src/main/scala/com/github/agourlay/cornichon/dsl/BodyElementCollector.scala
@@ -2,11 +2,9 @@ package com.github.agourlay.cornichon.dsl
 
 import scala.language.experimental.{ macros ⇒ `scalac, please just let me do it!` }
 
-import com.github.agourlay.cornichon.macros.Macro
-
 case class BodyElementCollector[Body, Result](fn: List[Body] ⇒ Result) {
-  def apply(body: ⇒ Body): Result = macro Macro.collectImpl
-  def apply(body: ⇒ Seq[Body]): Result = macro Macro.collectImpl
+  def apply(body: ⇒ Body): Result = macro BodyElementCollectorMacro.collectImpl
+  def apply(body: ⇒ Seq[Body]): Result = macro BodyElementCollectorMacro.collectImpl
 
   def get(body: List[Body]): Result = fn(body)
 }
