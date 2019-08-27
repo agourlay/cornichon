0707c3566882af7da5fce50fa7e099accd46864e
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2019-08-27 13:09:19 +0200

    cleanup taskSpec

--- cornichon-core/src/test/scala/com/github/agourlay/cornichon/util/TaskSpec.scala
+++ cornichon-core/src/test/scala/com/github/agourlay/cornichon/util/TaskSpec.scala
@@ -6,10 +6,6 @@ import monix.execution.Scheduler
 import scala.concurrent.Future
 
 trait TaskSpec {
-
   implicit val scheduler: Scheduler = Scheduler.Implicits.global
-
-  implicit def taskToFuture[A](t: Task[A])(implicit s: Scheduler): Future[A] =
-    t.runToFuture(s)
-
+  implicit def taskToFuture[A](t: Task[A]): Future[A] = t.runToFuture(scheduler)
 }
