e3191d89c654408c9e463a10c5e9b74721886ee3
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2019-08-28 17:14:55 +0200

    a few more unit test for EffectStep

--- cornichon-core/src/test/scala/com/github/agourlay/cornichon/steps/regular/EffectStepSpec.scala
+++ cornichon-core/src/test/scala/com/github/agourlay/cornichon/steps/regular/EffectStepSpec.scala
@@ -1,6 +1,6 @@
 package com.github.agourlay.cornichon.steps.regular
 
-import com.github.agourlay.cornichon.core.{ Scenario, ScenarioRunner, Session }
+import com.github.agourlay.cornichon.core.{ CornichonError, Scenario, ScenarioRunner, Session }
 import com.github.agourlay.cornichon.steps.StepUtilSpec
 import com.github.agourlay.cornichon.steps.cats.{ EffectStep ⇒ CEffectStep }
 import monix.eval.Task
@@ -26,6 +26,20 @@ class EffectStepSpec extends AsyncWordSpec with Matchers with StepUtilSpec {
         ScenarioRunner.runScenario(Session.newEmpty)(s).map(_.isSuccess should be(false))
       }
     }
+
+    "SyncE" must {
+      "valid if effect is an Either.Right" in {
+        val step = EffectStep.fromSyncE(title = "valid effect", sc ⇒ Right(sc.session))
+        val s = Scenario("scenario with valid effect step", step :: Nil)
+        ScenarioRunner.runScenario(Session.newEmpty)(s).map(_.isSuccess should be(true))
+      }
+
+      "invalid if effect is an Either.Left" in {
+        val step = EffectStep.fromSyncE(title = "valid effect", sc ⇒ Left(CornichonError.fromString("ohh nooes")))
+        val s = Scenario("scenario with invalid effect step", step :: Nil)
+        ScenarioRunner.runScenario(Session.newEmpty)(s).map(_.isSuccess should be(false))
+      }
+    }
   }
 
   "CatsEffectStep" when {
@@ -41,6 +55,20 @@ class EffectStepSpec extends AsyncWordSpec with Matchers with StepUtilSpec {
       "return error if an Effect step throw an exception" in {
         val step = CEffectStep[Task](title = "buggy effect", _ ⇒ throw new RuntimeException("boom"))
         val s = Scenario("scenario with broken effect step", step :: Nil)
+        ScenarioRunner.runScenario(Session.newEmpty)(s).map(_.isSuccess should be(false))
+      }
+    }
+
+    "SyncE" must {
+      "valid if effect is an Either.Right" in {
+        val step = CEffectStep.fromSyncE(title = "valid effect", sc ⇒ Right(sc.session))
+        val s = Scenario("scenario with valid effect step", step :: Nil)
+        ScenarioRunner.runScenario(Session.newEmpty)(s).map(_.isSuccess should be(true))
+      }
+
+      "invalid if effect is an Either.Left" in {
+        val step = CEffectStep.fromSyncE(title = "valid effect", sc ⇒ Left(CornichonError.fromString("ohh nooes")))
+        val s = Scenario("scenario with invalid effect step", step :: Nil)
         ScenarioRunner.runScenario(Session.newEmpty)(s).map(_.isSuccess should be(false))
       }
     }
