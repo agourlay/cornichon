52f5a9fee08f10e40742edab5bdbc70675a295f2
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2019-10-29 11:32:50 +0100

    more utest less scalatest

--- cornichon-core/src/test/scala/com/github/agourlay/cornichon/core/ScenarioRunnerSpec.scala
+++ cornichon-core/src/test/scala/com/github/agourlay/cornichon/core/ScenarioRunnerSpec.scala
@@ -5,235 +5,226 @@ import java.util.concurrent.atomic.AtomicInteger
 import com.github.agourlay.cornichon.dsl.ProvidedInstances._
 import com.github.agourlay.cornichon.steps.cats.EffectStep
 import com.github.agourlay.cornichon.steps.regular.assertStep.{ AssertStep, GenericEqualityAssertion }
-import com.github.agourlay.cornichon.util.{ ScenarioMatchers, TaskSpec }
-import org.scalatest.{ AsyncWordSpec, Matchers }
+import com.github.agourlay.cornichon.util.{ ScenarioMatchersUtest, TaskSpec }
+import utest._
 
-class ScenarioRunnerSpec extends AsyncWordSpec with Matchers with TaskSpec with ScenarioMatchers {
+object ScenarioRunnerSpec extends TestSuite with TaskSpec with ScenarioMatchersUtest {
 
-  "ScenarioRunner" when {
-    "runScenario" must {
-      "executes all steps of a scenario in case of success" in {
-        val beforeSteps = AssertStep("before assertion", _ => GenericEqualityAssertion(2 + 1, 3)) :: Nil
-        val steps = AssertStep("main assertion", _ => GenericEqualityAssertion(2 + 1, 3)) :: Nil
-        val finallySteps = AssertStep("finally assertion", _ => GenericEqualityAssertion(2 + 1, 3)) :: Nil
-        val fc = FeatureContext.empty.copy(
-          beforeSteps = beforeSteps,
-          finallySteps = finallySteps
-        )
-        val s = Scenario("casual stuff", steps)
-        ScenarioRunner.runScenario(Session.newEmpty, fc)(s).map { r =>
-          r.isSuccess should be(true)
-          matchLogsWithoutDuration(r.logs) {
-            """
-              |   Scenario : casual stuff
-              |      before steps
-              |      before assertion
-              |      main steps
-              |      main assertion
-              |      finally steps
-              |      finally assertion""".stripMargin
-          }
-        }
+  val tests = Tests {
+    test("runScenario executes all steps of a scenario in case of success") {
+      val beforeSteps = AssertStep("before assertion", _ => GenericEqualityAssertion(2 + 1, 3)) :: Nil
+      val steps = AssertStep("main assertion", _ => GenericEqualityAssertion(2 + 1, 3)) :: Nil
+      val finallySteps = AssertStep("finally assertion", _ => GenericEqualityAssertion(2 + 1, 3)) :: Nil
+      val fc = FeatureContext.empty.copy(
+        beforeSteps = beforeSteps,
+        finallySteps = finallySteps
+      )
+      val s = Scenario("casual stuff", steps)
+      val res = awaitTask(ScenarioRunner.runScenario(Session.newEmpty, fc)(s))
+      assert(res.isSuccess)
+      matchLogsWithoutDuration(res.logs) {
+        """
+            |   Scenario : casual stuff
+            |      before steps
+            |      before assertion
+            |      main steps
+            |      main assertion
+            |      finally steps
+            |      finally assertion""".stripMargin
+      }
+    }
+
+    test("runScenario does not run `main steps` if there is a failure in `beforeSteps`") {
+      val beforeSteps = AssertStep("before assertion", _ => GenericEqualityAssertion(2 + 1, 4)) :: Nil
+      val steps = AssertStep("main assertion", _ => GenericEqualityAssertion(2 + 1, 3)) :: Nil
+      val finallySteps = AssertStep("finally assertion", _ => GenericEqualityAssertion(2 + 1, 3)) :: Nil
+      val fc = FeatureContext.empty.copy(
+        beforeSteps = beforeSteps,
+        finallySteps = finallySteps
+      )
+      val s = Scenario("casual stuff", steps)
+      val res = awaitTask(ScenarioRunner.runScenario(Session.newEmpty, fc)(s))
+      scenarioFailsWithMessage(res) {
+        """Scenario 'casual stuff' failed:
+          |
+          |at step:
+          |before assertion
+          |
+          |with error(s):
+          |expected result was:
+          |'3'
+          |but actual result is:
+          |'4'
+          |
+          |seed for the run was '1'
+          |""".stripMargin
       }
 
-      "do not run `main steps` if there is a failure in `beforeSteps`" in {
-        val beforeSteps = AssertStep("before assertion", _ => GenericEqualityAssertion(2 + 1, 4)) :: Nil
-        val steps = AssertStep("main assertion", _ => GenericEqualityAssertion(2 + 1, 3)) :: Nil
-        val finallySteps = AssertStep("finally assertion", _ => GenericEqualityAssertion(2 + 1, 3)) :: Nil
-        val fc = FeatureContext.empty.copy(
-          beforeSteps = beforeSteps,
-          finallySteps = finallySteps
-        )
-        val s = Scenario("casual stuff", steps)
-        ScenarioRunner.runScenario(Session.newEmpty, fc)(s).map { res =>
-          scenarioFailsWithMessage(res) {
-            """Scenario 'casual stuff' failed:
-              |
-              |at step:
-              |before assertion
-              |
-              |with error(s):
-              |expected result was:
-              |'3'
-              |but actual result is:
-              |'4'
-              |
-              |seed for the run was '1'
-              |""".stripMargin
-          }
+      matchLogsWithoutDuration(res.logs) {
+        """
+          |   Scenario : casual stuff
+          |      before steps
+          |      before assertion
+          |      *** FAILED ***
+          |      expected result was:
+          |      '3'
+          |      but actual result is:
+          |      '4'
+          |      finally steps
+          |      finally assertion""".stripMargin
+      }
+    }
 
-          matchLogsWithoutDuration(res.logs) {
-            """
-              |   Scenario : casual stuff
-              |      before steps
-              |      before assertion
-              |      *** FAILED ***
-              |      expected result was:
-              |      '3'
-              |      but actual result is:
-              |      '4'
-              |      finally steps
-              |      finally assertion""".stripMargin
-          }
-        }
+    test("runScenario stops at first failed step") {
+      val step1 = AssertStep("first step", _ => GenericEqualityAssertion(2, 2))
+      val step2 = AssertStep("second step", _ => GenericEqualityAssertion(4, 5))
+      val step3 = AssertStep("third step", _ => GenericEqualityAssertion(1, 1))
+      val steps = step1 :: step2 :: step3 :: Nil
+      val s = Scenario("early stop", steps)
+      val res = awaitTask(ScenarioRunner.runScenario(Session.newEmpty)(s))
+      scenarioFailsWithMessage(res) {
+        """Scenario 'early stop' failed:
+          |
+          |at step:
+          |second step
+          |
+          |with error(s):
+          |expected result was:
+          |'4'
+          |but actual result is:
+          |'5'
+          |
+          |seed for the run was '1'
+          |""".stripMargin
       }
 
-      "stops at first failed step" in {
-        val step1 = AssertStep("first step", _ => GenericEqualityAssertion(2, 2))
-        val step2 = AssertStep("second step", _ => GenericEqualityAssertion(4, 5))
-        val step3 = AssertStep("third step", _ => GenericEqualityAssertion(1, 1))
-        val steps = step1 :: step2 :: step3 :: Nil
-        val s = Scenario("early stop", steps)
-        ScenarioRunner.runScenario(Session.newEmpty)(s).map { res =>
-          scenarioFailsWithMessage(res) {
-            """Scenario 'early stop' failed:
-              |
-              |at step:
-              |second step
-              |
-              |with error(s):
-              |expected result was:
-              |'4'
-              |but actual result is:
-              |'5'
-              |
-              |seed for the run was '1'
-              |""".stripMargin
-          }
+      matchLogsWithoutDuration(res.logs) {
+        """
+          |   Scenario : early stop
+          |      main steps
+          |      first step
+          |      second step
+          |      *** FAILED ***
+          |      expected result was:
+          |      '4'
+          |      but actual result is:
+          |      '5'""".stripMargin
+      }
+    }
 
-          matchLogsWithoutDuration(res.logs) {
-            """
-              |   Scenario : early stop
-              |      main steps
-              |      first step
-              |      second step
-              |      *** FAILED ***
-              |      expected result was:
-              |      '4'
-              |      but actual result is:
-              |      '5'""".stripMargin
-          }
-        }
+    test("runScenario accumulates errors if 'main' and 'finally' fail") {
+      val mainStep = AssertStep("main assertion", _ => GenericEqualityAssertion(true, false))
+      val finalAssertion = AssertStep("finally assertion", _ => GenericEqualityAssertion(true, false))
+      val s = Scenario("accumulate", mainStep :: Nil)
+      val fc = FeatureContext.empty.copy(finallySteps = finalAssertion :: Nil, withSeed = Some(1))
+      val res = awaitTask(ScenarioRunner.runScenario(Session.newEmpty, fc)(s))
+      scenarioFailsWithMessage(res) {
+        """Scenario 'accumulate' failed:
+          |
+          |at step:
+          |main assertion
+          |
+          |with error(s):
+          |expected result was:
+          |'true'
+          |but actual result is:
+          |'false'
+          |
+          |and
+          |
+          |at step:
+          |finally assertion
+          |
+          |with error(s):
+          |expected result was:
+          |'true'
+          |but actual result is:
+          |'false'
+          |
+          |seed for the run was '1'
+          |""".stripMargin
       }
 
-      "accumulates errors if 'main' and 'finally' fail" in {
-        val mainStep = AssertStep("main assertion", _ => GenericEqualityAssertion(true, false))
-        val finalAssertion = AssertStep("finally assertion", _ => GenericEqualityAssertion(true, false))
-        val s = Scenario("accumulate", mainStep :: Nil)
-        val fc = FeatureContext.empty.copy(finallySteps = finalAssertion :: Nil, withSeed = Some(1))
-        ScenarioRunner.runScenario(Session.newEmpty, fc)(s).map { r =>
-          scenarioFailsWithMessage(r) {
-            """Scenario 'accumulate' failed:
-              |
-              |at step:
-              |main assertion
-              |
-              |with error(s):
-              |expected result was:
-              |'true'
-              |but actual result is:
-              |'false'
-              |
-              |and
-              |
-              |at step:
-              |finally assertion
-              |
-              |with error(s):
-              |expected result was:
-              |'true'
-              |but actual result is:
-              |'false'
-              |
-              |seed for the run was '1'
-              |""".stripMargin
-          }
-
-          matchLogsWithoutDuration(r.logs) {
-            """
-              |   Scenario : accumulate
-              |      main steps
-              |      main assertion
-              |      *** FAILED ***
-              |      expected result was:
-              |      'true'
-              |      but actual result is:
-              |      'false'
-              |      finally steps
-              |      finally assertion
-              |      *** FAILED ***
-              |      expected result was:
-              |      'true'
-              |      but actual result is:
-              |      'false'""".stripMargin
-          }
-        }
+      matchLogsWithoutDuration(res.logs) {
+        """
+          |   Scenario : accumulate
+          |      main steps
+          |      main assertion
+          |      *** FAILED ***
+          |      expected result was:
+          |      'true'
+          |      but actual result is:
+          |      'false'
+          |      finally steps
+          |      finally assertion
+          |      *** FAILED ***
+          |      expected result was:
+          |      'true'
+          |      but actual result is:
+          |      'false'""".stripMargin
       }
+    }
 
-      "run all valid effects" in {
-        val uglyCounter = new AtomicInteger(0)
-        val effectNumber = 5
-        val effect = EffectStep.fromSync(
-          "increment captured counter",
-          sc => {
-            uglyCounter.incrementAndGet()
-            sc.session
-          }
-        )
-
-        val s = Scenario("scenario with effects", List.fill(effectNumber)(effect))
-        ScenarioRunner.runScenario(Session.newEmpty)(s).map { res =>
-          res.isSuccess should be(true)
-          uglyCounter.get() should be(effectNumber)
-          res.logs.size should be(effectNumber + 2)
+    test("runScenario runs all valid effects") {
+      val uglyCounter = new AtomicInteger(0)
+      val effectNumber = 5
+      val effect = EffectStep.fromSync(
+        "increment captured counter",
+        sc => {
+          uglyCounter.incrementAndGet()
+          sc.session
         }
-      }
+      )
 
-      "run all valid main effects and finally effects" in {
-        val uglyCounter = new AtomicInteger(0)
-        val effectNumber = 5
-        val effect = EffectStep.fromSync(
-          "increment captured counter",
-          sc => {
-            uglyCounter.incrementAndGet()
-            sc.session
-          }
-        )
+      val s = Scenario("scenario with effects", List.fill(effectNumber)(effect))
+      val res = awaitTask(ScenarioRunner.runScenario(Session.newEmpty)(s))
+      assert(res.isSuccess)
+      assert(uglyCounter.get() == effectNumber)
+      assert(res.logs.size == effectNumber + 2)
+    }
 
-        val context = FeatureContext.empty.copy(finallySteps = List.fill(effectNumber)(effect))
-        val s = Scenario("scenario with effects", context.finallySteps)
-        ScenarioRunner.runScenario(Session.newEmpty, context)(s).map { res =>
-          res.isSuccess should be(true)
-          uglyCounter.get() should be(effectNumber * 2)
-          res.logs.size should be(effectNumber * 2 + 3)
+    test("runScenario runs all valid main effects and finally effects") {
+      val uglyCounter = new AtomicInteger(0)
+      val effectNumber = 5
+      val effect = EffectStep.fromSync(
+        "increment captured counter",
+        sc => {
+          uglyCounter.incrementAndGet()
+          sc.session
         }
-      }
+      )
 
-      "have a deterministic run via a fixed seed" in {
-        val fixedTestSeed = 12345L
-        val fc = FeatureContext.empty.copy(withSeed = Some(fixedTestSeed))
+      val context = FeatureContext.empty.copy(finallySteps = List.fill(effectNumber)(effect))
+      val s = Scenario("scenario with effects", context.finallySteps)
+      val res = awaitTask(ScenarioRunner.runScenario(Session.newEmpty, context)(s))
+      assert(res.isSuccess)
+      assert(uglyCounter.get() == effectNumber * 2)
+      assert(res.logs.size == effectNumber * 2 + 3)
+    }
 
-        val assertSeed = AssertStep("assert seed", sc => {
-          GenericEqualityAssertion(sc.randomContext.initialSeed, fixedTestSeed)
-        })
+    test("runScenario has deterministic runs via a fixed seed") {
+      val fixedTestSeed = 12345L
+      val fc = FeatureContext.empty.copy(withSeed = Some(fixedTestSeed))
 
-        val rdStep = EffectStep.fromSyncE("pick random int", sc => {
-          val rdInt = sc.randomContext.seededRandom.nextInt()
-          sc.session.addValue("random-int", rdInt.toString)
-        })
+      val assertSeed = AssertStep("assert seed", sc => {
+        GenericEqualityAssertion(sc.randomContext.initialSeed, fixedTestSeed)
+      })
 
-        val rdAssert = AssertStep("assert rd", sc => {
-          val rdValue = sc.session.getUnsafe("random-int")
-          GenericEqualityAssertion(rdValue, "1553932502") // value generated with the fixedTestSeed
-        })
+      val rdStep = EffectStep.fromSyncE("pick random int", sc => {
+        val rdInt = sc.randomContext.seededRandom.nextInt()
+        sc.session.addValue("random-int", rdInt.toString)
+      })
 
-        val steps = assertSeed :: rdStep :: rdAssert :: Nil
-        val s = Scenario("deterministic test", steps)
-        ScenarioRunner.runScenario(Session.newEmpty, fc)(s).map { r =>
-          r.isSuccess should be(true)
-          r.logs.size should be(5)
-        }
-      }
+      val rdAssert = AssertStep("assert rd", sc => {
+        val rdValue = sc.session.getUnsafe("random-int")
+        GenericEqualityAssertion(rdValue, "1553932502") // value generated with the fixedTestSeed
+      })
+
+      val steps = assertSeed :: rdStep :: rdAssert :: Nil
+      val s = Scenario("deterministic test", steps)
+      val res = awaitTask(ScenarioRunner.runScenario(Session.newEmpty, fc)(s))
+      assert(res.isSuccess)
+      assert(res.logs.size == 5)
     }
   }
 }
--- cornichon-core/src/test/scala/com/github/agourlay/cornichon/steps/regular/DebugStepSpec.scala
+++ cornichon-core/src/test/scala/com/github/agourlay/cornichon/steps/regular/DebugStepSpec.scala
@@ -2,82 +2,79 @@ package com.github.agourlay.cornichon.steps.regular
 
 import com.github.agourlay.cornichon.core.{ CornichonError, Scenario, ScenarioRunner, Session }
 import com.github.agourlay.cornichon.steps.StepUtilSpec
-import com.github.agourlay.cornichon.util.ScenarioMatchers
-import org.scalatest.{ AsyncWordSpec, Matchers }
+import com.github.agourlay.cornichon.util.ScenarioMatchersUtest
+import utest._
 
 import scala.util.control.NoStackTrace
 
-class DebugStepSpec extends AsyncWordSpec with Matchers with StepUtilSpec with ScenarioMatchers {
+object DebugStepSpec extends TestSuite with StepUtilSpec with ScenarioMatchersUtest {
 
-  "DebugStep" must {
-    "fail scenario if a Debug step throw an exception" in {
+  val tests = Tests {
+    test("fails scenario if a Debug step throws an exception") {
       val step = DebugStep("bad debug", _ => throw new RuntimeException("boom") with NoStackTrace)
       val s = Scenario("scenario with faulty debug step", step :: Nil)
-      ScenarioRunner.runScenario(Session.newEmpty)(s).map { rep =>
-        scenarioFailsWithMessage(rep) {
-          """Scenario 'scenario with faulty debug step' failed:
-            |
-            |at step:
-            |bad debug
-            |
-            |with error(s):
-            |exception thrown com.github.agourlay.cornichon.steps.regular.DebugStepSpec$$anon$1: boom
-            |
-            |
-            |seed for the run was '1'
-            |""".stripMargin
-        }
+      val res = awaitTask(ScenarioRunner.runScenario(Session.newEmpty)(s))
+      scenarioFailsWithMessage(res) {
+        """Scenario 'scenario with faulty debug step' failed:
+          |
+          |at step:
+          |bad debug
+          |
+          |with error(s):
+          |exception thrown com.github.agourlay.cornichon.steps.regular.DebugStepSpec$$anon$1: boom
+          |
+          |
+          |seed for the run was '1'
+          |""".stripMargin
+      }
 
-        matchLogsWithoutDuration(rep.logs) {
-          """
-            |   Scenario : scenario with faulty debug step
-            |      main steps
-            |      bad debug
-            |      *** FAILED ***
-            |      exception thrown com.github.agourlay.cornichon.steps.regular.DebugStepSpec$$anon$1: boom""".stripMargin
-        }
+      matchLogsWithoutDuration(res.logs) {
+        """
+          |   Scenario : scenario with faulty debug step
+          |      main steps
+          |      bad debug
+          |      *** FAILED ***
+          |      exception thrown com.github.agourlay.cornichon.steps.regular.DebugStepSpec$$anon$1: boom""".stripMargin
       }
     }
 
-    "fail scenario if a Debug step return an Either.Left" in {
+    test("fails scenario if a Debug step returns an Either.Left") {
       val step = DebugStep("invalid debug", _ => Left(CornichonError.fromString("debugging with println went wrong!")))
       val s = Scenario("scenario with faulty debug step", step :: Nil)
-      ScenarioRunner.runScenario(Session.newEmpty)(s).map { rep =>
-        scenarioFailsWithMessage(rep) {
-          """Scenario 'scenario with faulty debug step' failed:
-            |
-            |at step:
-            |invalid debug
-            |
-            |with error(s):
-            |debugging with println went wrong!
-            |
-            |seed for the run was '1'
-            |""".stripMargin
-        }
+      val res = awaitTask(ScenarioRunner.runScenario(Session.newEmpty)(s))
+      scenarioFailsWithMessage(res) {
+        """Scenario 'scenario with faulty debug step' failed:
+          |
+          |at step:
+          |invalid debug
+          |
+          |with error(s):
+          |debugging with println went wrong!
+          |
+          |seed for the run was '1'
+          |""".stripMargin
+      }
 
-        matchLogsWithoutDuration(rep.logs) {
-          """
-            |   Scenario : scenario with faulty debug step
-            |      main steps
-            |      invalid debug
-            |      *** FAILED ***
-            |      debugging with println went wrong!""".stripMargin
-        }
+      matchLogsWithoutDuration(res.logs) {
+        """
+          |   Scenario : scenario with faulty debug step
+          |      main steps
+          |      invalid debug
+          |      *** FAILED ***
+          |      debugging with println went wrong!""".stripMargin
       }
     }
 
-    "debug info is present in the logs" in {
+    test("debug info is present in the logs") {
       val step = DebugStep("debug info", _ => Right("debugging with println"))
       val s = Scenario("scenario with correct debug step", step :: Nil)
-      ScenarioRunner.runScenario(Session.newEmpty)(s).map { rep =>
-        rep.isSuccess should be(true)
-        matchLogsWithoutDuration(rep.logs) {
-          """
-            |   Scenario : scenario with correct debug step
-            |      main steps
-            |      debugging with println""".stripMargin
-        }
+      val res = awaitTask(ScenarioRunner.runScenario(Session.newEmpty)(s))
+      assert(res.isSuccess)
+      matchLogsWithoutDuration(res.logs) {
+        """
+        |   Scenario : scenario with correct debug step
+        |      main steps
+        |      debugging with println""".stripMargin
       }
     }
   }
