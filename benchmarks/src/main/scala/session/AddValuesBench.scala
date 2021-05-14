6188ead8059167059bc369a63412ef424e3191be
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2021-05-14 21:03:48 +0200

    update and improve AddValuesBench

--- benchmarks/src/main/scala/session/AddValuesBench.scala
+++ benchmarks/src/main/scala/session/AddValuesBench.scala
@@ -6,7 +6,7 @@ import session.AddValuesBench._
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Array(Mode.Throughput))
-@Warmup(iterations = 10)
+@Warmup(iterations = 5)
 @Measurement(iterations = 10)
 @Fork(value = 1, jvmArgsAppend = Array(
   "-XX:+FlightRecorder",
@@ -21,21 +21,36 @@ class AddValuesBench {
 
   /*
   [info] Benchmark                 (insertNumber)   Mode  Cnt        Score       Error  Units
-  [info] AddValuesBench.addValues               1  thrpt   10  6139574,714 ± 17220,088  ops/s
-  [info] AddValuesBench.addValues               2  thrpt   10  2720783,933 ±  7727,835  ops/s
-  [info] AddValuesBench.addValues               3  thrpt   10  1699018,869 ±  1625,251  ops/s
-  [info] AddValuesBench.addValues               4  thrpt   10  1301837,618 ±  3594,495  ops/s
-  [info] AddValuesBench.addValues               5  thrpt   10  1098475,777 ±  3929,211  ops/s
-  [info] AddValuesBench.addValues              10  thrpt   10   548834,559 ±  2547,712  ops/s
+  [info] AddValuesBench.addValues               1  thrpt   10  7134780,331 ± 36314,364  ops/s
+  [info] AddValuesBench.addValues               2  thrpt   10  2865439,318 ± 14360,784  ops/s
+  [info] AddValuesBench.addValues               3  thrpt   10  1538325,303 ±  7640,665  ops/s
+  [info] AddValuesBench.addValues               4  thrpt   10  1251350,207 ±  6425,952  ops/s
+  [info] AddValuesBench.addValues               5  thrpt   10  1042564,281 ± 11919,693  ops/s
+  [info] AddValuesBench.addValues              10  thrpt   10   566898,054 ±  2816,408  ops/s
   */
   @Benchmark
   def addValues() = {
-    val values = List.fill(insertNumber.toInt)(tupleEntry)
-    Session.newEmpty.addValues(values: _*)
+    val values = insertNumber match {
+      case "1" => oneEntry
+      case "2" => twoEntries
+      case "3" => threeEntries
+      case "4" => fourEntries
+      case "5" => fiveEntries
+      case "10" => tenEntries
+    }
+    val s2 = Session.newEmpty.addValues(values: _*)
+    assert(s2.isRight)
   }
 }
 
 object AddValuesBench {
   val tupleEntry = "key" -> "value"
+
+  val oneEntry = tupleEntry :: Nil
+  val twoEntries = List.fill(2)(tupleEntry)
+  val threeEntries = List.fill(3)(tupleEntry)
+  val fourEntries = List.fill(4)(tupleEntry)
+  val fiveEntries = List.fill(5)(tupleEntry)
+  val tenEntries = List.fill(10)(tupleEntry)
 }
 
